18def6baa46e138571340afcf06cfc804e783a6f
Alex Coleman <a.coleman1@leeds.ac.uk>
2020-03-06 09:45:08 +0000

    updated comments on main functions

--- getAverages.m
+++ getAverages.m
@@ -1,14 +1,25 @@
 function getAvg = getAverages(datafile, filename)
 
 % simple function to calculate averages of datatable
+% load the datafile outputs as a struct
 data = load(datafile)
 
+% calculate the mean of the single col of data
+% have to convert struct into an array (here as two step struct->table->array
 result = mean(table2array(struct2array(data)))
 
+% get environment variable 
+% in this case the task array task ID (number of task)
 task_no = getenv('$SGE_TASK_ID')
 
+% create a filename string variable
+% use strcat here to concatenate strings
+% filename passed string, and _, the task number and then the file extension
 outname = strcat(filename,'_',task_no,'.mat')
 
+% save the result variable created above
+% with the name generated in outname
 save(outname, 'result')
 
+% end of function
 end
--- task_matlab.sh
+++ task_matlab.sh
@@ -6,9 +6,18 @@
 #$ -l h_vmem=1G
 #$ -t 1-3
 
+# use sed to get the filename from files_to_do.txt
+# based on the task number
+# i.e. task 1 will select data1.mat
 infile=$(sed -n -e "$SGE_TASK_ID p" files_to_do.txt)
 
 # start code
+# load matlab
 module load matlab
 
+# invoke matlab with nodisplay option 
+# -r flag means we take a string as an argument and run it in matlab
+# in this case run the getAverages function
+# passing file name specified above as infile variable as parameters
+# getAverages expects arg1=path to data, arg2=name for output file
 matlab -nodisplay -r "getAverages('data/$infile','${infile%.mat}');exit"
